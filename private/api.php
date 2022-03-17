33517b63883505476f7e6d7b741223aa9a8755d4
tomuk91 <36379416+tomuk91@users.noreply.github.com>
2022-03-17 14:54:12 +0000

    installed npm and bootstrap, updated styling

--- private/api.php
+++ private/api.php
@@ -2,18 +2,46 @@
 
 require_once 'init.php';
 
-function getWeatherData(string $city) {
-    $url = "https://api.openweathermap.org/data/2.5/weather?q=" . $city  . "&appid=" . APIKey . "&units=metric";
-    $curl = curl_init();
+function getWeatherData(string $city)
+{
+	$url = "https://api.openweathermap.org/data/2.5/weather?q=" . $city . "&appid=" . APIKey . "&units=metric";
+	$curl = curl_init();
 
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($curl, CURLOPT_HEADER, FALSE);
 
-    $weather = curl_exec($curl);
+	$weather = curl_exec($curl);
 
-    curl_close($curl);
+	curl_close($curl);
 
-    return json_decode($weather, true);
+	return json_decode($weather, TRUE);
 }
 
+
+function dailyWeatherData(string $city)
+{
+
+	$curl = curl_init();
+
+	curl_setopt_array($curl, [
+		CURLOPT_URL => "https://community-open-weather-map.p.rapidapi.com/forecast?q=" . $city . "&units=metric",
+		CURLOPT_RETURNTRANSFER => TRUE,
+		CURLOPT_FOLLOWLOCATION => TRUE,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => [
+			"x-rapidapi-host: community-open-weather-map.p.rapidapi.com",
+			"x-rapidapi-key: f3dbc9c5a2mshcc05cf4061f807bp1c12fajsn21f968aa0a9d"
+		],
+	]);
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+}
--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
     <title>Weather App</title>
 </head>
 <body>
@@ -16,50 +17,46 @@
 $city = 'london';
 $celsius = " °C";
 
-if (isset($_GET['searchCity'])) {
-    $city = $_GET['searchCity'];
+if (isset($_GET['searchCity']))
+{
+	$city = $_GET['searchCity'];
 }
 
-include(SHARED_PATH . '/background.php')
+include SHARED_PATH . '/background.php';
+include SHARED_PATH . '/nav.php';
 
 ?>
 
-<?php $weather = getWeatherData($city);
-var_dump($weather); ?>
+<?php $weather = getWeatherData($city); ?>
 
-<div class="weather-container">
-    <div class="title">
-        <h1><?php echo $weather["sys"]["country"] . " " . $weather["name"] ?></h1>
-        &nbsp
-        <h1 class="temp"><?php echo $weather["main"]["temp"] . $celsius ?></h1>
-    </div>
-    <div class="description">
-        <h2><?php echo ucfirst($weather["weather"]['0']["description"]) . " with a real feel of" . " " .
-                "<span class='temp'>" . $weather["main"]["feels_like"] . $celsius . "</span>" ?>
-        </h2>
-    </div>
-    <div class="stats">
-        <div class="minmax">
-            <h3>Min: <?php echo $weather["main"]["temp_min"] . $celsius ?></h3>
-            <h3>Max: <?php echo $weather["main"]["temp_max"] . $celsius ?></h3>
-            <h3>Wind: <?php echo $weather["wind"]["speed"] ?></h3>
-
+<div class="container banner">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="weather-image">
+                <img src="http://openweathermap.org/img/wn/<?php echo $weather["weather"]["0"]["icon"] ?>@2x.png">
+            </div>
         </div>
-        <div class="extrastats">
+        <div class="col-md-8">
+            <div class="title">
+                <h1><?php echo $weather["sys"]["country"] . " " . $weather["name"] ?></h1>
+                &nbsp
+                <h1 class="temp"><?php echo $weather["main"]["temp"] . $celsius ?></h1>
+            </div>
+            <div class="description">
+                <h2><?php echo ucfirst($weather["weather"]['0']["description"]) . " with a real feel of" . " " . "<span class='temp'>" . $weather["main"]["feels_like"] . $celsius . "</span>" ?>
+                </h2>
+            </div>
+            <div class="stats">
+                <h6><span class="stats-title">Humidity:</span> <?php echo $weather["main"]["humidity"] . "%" ?>
+                </h6>
+                <h6><span class="stats-title mx-1">Wind:</span><?php echo $weather["wind"]["speed"] . "MPH" ?></h6>
+                <h6><span class="stats-title mx-1">Pressure:</span><?php echo $weather["main"]["pressure"] . "hPa" ?>
+                </h6>
+            </div>
         </div>
-
     </div>
 </div>
 
-<form id="form">
-    <input type="search" id="query" name="searchCity" placeholder="Enter a city...">
-    <button>
-        <svg viewBox="0 0 1024 1024">
-            <path class="path1"
-                  d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path>
-        </svg>
-    </button>
-</form>
 
 </body>
 </html>
